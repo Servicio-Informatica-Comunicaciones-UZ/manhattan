7944a8ade2119c06058345442b1ce6715560c19d
Enrique Matías Sánchez (Quique) <quique@unizar.es>
2019-03-11 14:58:36 +0100

    fix Tipos de los campos de los modelos

--- indo/models.py
+++ indo/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
 
 
 class Centro(models.Model):
@@ -16,27 +15,22 @@ class Centro(models.Model):
     direccion = models.CharField("dirección", max_length=140, blank=True, null=True)
     municipio = models.CharField(max_length=100, blank=True, null=True)
     telefono = models.CharField("teléfono", max_length=30, blank=True, null=True)
-    email = models.CharField(
-        "correo electrónico", max_length=254, blank=True, null=True
-    )
-    url = models.CharField("URL", max_length=255, blank=True, null=True)
+    email = models.EmailField(_("email address"), blank=True, null=True)
+    url = models.URLField("URL", max_length=255, blank=True, null=True)
     nip_decano = models.PositiveIntegerField(blank=True, null=True)
     nombre_decano = models.CharField(max_length=255, blank=True, null=True)
-    email_decano = models.CharField(max_length=254, blank=True, null=True)
+    email_decano = models.EmailField(blank=True, null=True)
     tratamiento_decano = models.CharField(max_length=25, blank=True, null=True)
     nip_secretario = models.PositiveIntegerField(blank=True, null=True)
     nombre_secretario = models.CharField(max_length=255, blank=True, null=True)
-    email_secretario = models.CharField(max_length=254, blank=True, null=True)
+    email_secretario = models.EmailField(blank=True, null=True)
     esta_activo = models.BooleanField("¿Activo?", default=False)
 
     def __str__(self):
         return f"{self.academico_id_nk} {self.rrhh_id_nk} {self.nombre}"
 
     class Meta:
-        indexes = [
-            models.Index(fields=["academico_id_nk"]),
-            models.Index(fields=["rrhh_id_nk"]),
-        ]
+        indexes = [models.Index(fields=["academico_id_nk"])]
 
 
 class Departamento(models.Model):
@@ -48,13 +42,11 @@ class Departamento(models.Model):
         "cód. RRHH", max_length=4, blank=True, null=True, unique=True
     )
     nombre = models.CharField(max_length=255, blank=True, null=True)
-    email = models.CharField(
-        "correo electrónico", max_length=254, blank=True, null=True
-    )
-    email_secretaria = models.CharField(max_length=254, blank=True, null=True)
+    email = models.EmailField(_("email address"), blank=True, null=True)
+    email_secretaria = models.EmailField(blank=True, null=True)
     nip_director = models.PositiveIntegerField(blank=True, null=True)
     nombre_director = models.CharField(max_length=255, blank=True, null=True)
-    email_director = models.CharField(max_length=254, blank=True, null=True)
+    email_director = models.EmailField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.academico_id_nk} {self.rrhh_id_nk} {self.nombre}"
