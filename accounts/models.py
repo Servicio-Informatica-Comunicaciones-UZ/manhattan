13bb23b9386c2f428264a0a7f5b1fcf2ea390c6e
Enrique Matías Sánchez (Quique) <quique@unizar.es>
2019-03-13 12:12:17 +0100

    enh Un usuario puede tener varios centros y departamentos

--- accounts/models.py
+++ accounts/models.py
@@ -4,10 +4,11 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
-    # add additional fields in here
+    # Campos sobrescritos
     first_name = models.CharField(
-        _("first name"), max_length=50, blank=True
-    )  # era: max_length=30
+        _("first name"), max_length=50, blank=True  # era: max_length=30
+    )
+    # Campos adicionales
     numero_documento = models.CharField(
         _("número de documento"),
         max_length=16,
@@ -28,10 +29,21 @@ class CustomUser(AbstractUser):
     sexo = models.CharField(max_length=1, blank=True, null=True)
     sexo_oficial = models.CharField(max_length=1, blank=True, null=True)
     nombre_oficial = models.CharField(max_length=50, blank=True, null=True)
-    centro = models.ForeignKey("indo.Centro", models.DO_NOTHING, blank=True, null=True)
-    departamento = models.ForeignKey(
-        "indo.Departamento", models.DO_NOTHING, blank=True, null=True
+    centro_id_nks = models.CharField(
+        _("Cód. centros"), max_length=127, blank=True, null=True
+    )
+    departamento_id_nks = models.CharField(
+        _("Cód. departamentos"), max_length=127, blank=True, null=True
     )
 
+    # Metodos sobrescritos
+    def get_full_name(self):
+        """
+        Devuelve el nombre completo (nombre y los dos apellidos).
+        """
+        full_name = "%s %s %s" % (self.first_name, self.last_name, self.last_name_2)
+        return full_name.strip()
+
+    # Métodos adicionales
     def __str__(self):
         return self.username
