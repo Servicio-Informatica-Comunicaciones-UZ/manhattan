35fdbd51f8167efcb6a4682346a252f4471b777d
Enrique Matías Sánchez (Quique) <cronopios@gmail.com>
2019-06-03 13:37:27 +0200

    test Tests para probar GitLab CI

--- indo/tests.py
+++ indo/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse("home"))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse("home"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "home.html")
+
+
+class AyudaTest(SimpleTestCase):
+    def test_view_url_exists_at_proper_location(self):
+        response = self.client.get("/ayuda/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse("ayuda"))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse("ayuda"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "ayuda.html")
