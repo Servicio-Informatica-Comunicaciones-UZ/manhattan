691cc5116cb72fe367890069eee252c58941e67e
Enrique Matías Sánchez (Quique) <quique@unizar.es>
2019-06-07 08:49:37 +0200

    test Usar coverage, y 2 nuevos tests

--- indo/tests.py
+++ indo/tests.py
@@ -1,8 +1,14 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+from .views import HomePageView
 
 
 class HomeTests(SimpleTestCase):
+    def test_root_url_resolves_to_homepage_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func.__name__, HomePageView.as_view().__name__)
+        self.assertEqual(found.view_name, "home")
+
     def test_home_page_status_code(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
@@ -13,8 +19,14 @@ class HomeTests(SimpleTestCase):
 
     def test_view_uses_correct_template(self):
         resp = self.client.get(reverse("home"))
-        self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, "home.html")
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get("/")
+        html = response.content.decode("utf-8")
+        self.assertTrue(html.startswith("<!DOCTYPE html>"))
+        self.assertIn("<title>Proyectos de Innovación Docente</title>", html)
+        self.assertTrue(html.endswith("</html>\n"))
 
 
 class AyudaTest(SimpleTestCase):
@@ -28,5 +40,4 @@ class AyudaTest(SimpleTestCase):
 
     def test_view_uses_correct_template(self):
         resp = self.client.get(reverse("ayuda"))
-        self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, "ayuda.html")
