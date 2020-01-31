79d55100e95131afb3d286b8a7a4a9179f9b7136
Enrique Matías Sánchez (Quique) <quique@unizar.es>
2020-01-31 13:46:45 +0100

    fix Añadir filtro faltante `has_group`.

--- indo/templatetags/custom_tags.py
+++ indo/templatetags/custom_tags.py
@@ -37,6 +37,7 @@ def alert_style(tag):
 
 @register.simple_tag
 def lord_url():
+    """Devuelve la URL del Single Sign On."""
     return "{base}?{params}".format(
         base=reverse("social:begin", kwargs={"backend": "saml"}),
         params=urllib.parse.urlencode({"next": "/", "idp": "lord"}),
@@ -45,11 +46,17 @@ def lord_url():
 
 @register.filter
 def get_obj_attr(obj, attr):
-    """Devuelve el valor del atributo `attr` del objeto `obj`"""
+    """Devuelve el valor del atributo `attr` del objeto `obj`."""
     return getattr(obj, attr)
 
 
 @register.filter
 def get_attr_verbose_name(obj, attr):
-    """Devuelve el nombre prolijo del atributo indicado"""
+    """Devuelve el nombre prolijo del atributo indicado."""
     return obj._meta.get_field(attr).verbose_name
+
+
+@register.filter(name="has_group")
+def has_group(user, group_name):
+    """Comprueba si el usuario pertenece al grupo indicado."""
+    return user.groups.filter(name=group_name).exists()
