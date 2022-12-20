0488eed328d0d5144ac0ef70e33785d04c921a51
Enrique Matías Sánchez (Quique) <cronopios@gmail.com>
2022-12-20 10:39:54 +0100

    chore Update Django to 4.1.4

--- manhattan_project/settings.py
+++ manhattan_project/settings.py
@@ -318,7 +318,7 @@ if DEBUG:
     # DJANGO-DEBUG-TOOLBAR - <https://github.com/jazzband/django-debug-toolbar>
     # ------------------------------------------------------------------------------
     # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-    INSTALLED_APPS += ['debug_toolbar']  # noqa F405
+    INSTALLED_APPS += ['debug_toolbar', 'django_fastdev']  # noqa F405
     # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
     MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
     # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
